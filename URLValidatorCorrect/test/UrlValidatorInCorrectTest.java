89a0aa07291262a8ae56f97631719fab0e0d89e7
donoghuc <cas.donoghue@gmail.com>
2018-03-10 11:35:10 -0800

    refactor manual tests 1-3 to show the three error conditions, show passing in Correct, failing in InCorrect

--- URLValidatorCorrect/test/UrlValidatorInCorrectTest.java
+++ URLValidatorCorrect/test/UrlValidatorInCorrectTest.java
@@ -4,6 +4,7 @@
 
 import junit.framework.TestCase;
 
+
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
@@ -19,23 +20,34 @@ public class UrlValidatorInCorrectTest extends TestCase {
       super(testName);
    }
 
-   private void printExpected(String url, boolean result_expected, boolean result_observed) {
-	   System.out.println(result_observed);
+   private int printExpected(String url, boolean result_expected, boolean result_observed) {
+	   //allow all test cases to run without hitting the assertion errors that stop test exectuion
+	   // print out the URL and the PASS FAIL
+//	   System.out.println(result_observed);
 	   if (result_expected == result_observed) {
 		   System.out.println(url + ": PASSED");
+		   return 0;
 	   }
 	   else {
 		   System.out.println(url + ": FAILED");
+		   return 1;
 	   }
    }
    
    public void testManualTest_1()
    {
 	   // This test shows the regex bug, basically anythin that is a "nested" link is falsely identified as invalid
+	   System.out.println("manual test 1");
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   assertTrue(urlVal.isValid("http://www.google.com"));
-	   assertTrue(urlVal.isValid("http://www.google.com/thing/"));
-	   assertTrue(urlVal.isValid("http://www.google.com/thing/another"));
+	   int test_fails = 0;
+	   test_fails += printExpected("http://www.google.com",true,urlVal.isValid("http://www.google.com"));
+	   test_fails += printExpected("http://www.google.com/thing/",true,urlVal.isValid("http://www.google.com/thing/"));
+	   test_fails += printExpected("http://www.google.com/thing/another",true,urlVal.isValid("http://www.google.com/thing/another"));
+	   assertTrue(test_fails == 0);
+//	   assertTrue(urlVal.isValid("http://www.google.com"));
+//	   assertTrue(urlVal.isValid("http://www.google.com/thing/"));
+//	   assertTrue(urlVal.isValid("http://www.google.com/thing/another"));
+	   
 	   
    }
    
@@ -43,21 +55,32 @@ public class UrlValidatorInCorrectTest extends TestCase {
    {   //This test shows the toLower _ toUpper bug
 	   // should be able to define custom schemes. in this case we allow only "http" and "bad" 
 	   // this implies that "https" should not be allowed
-	   System.out.println("manual test");
+	   System.out.println("manual test 2");
 	   String[] schemes = {"http","bad"};
 	   UrlValidator urlVal = new UrlValidator(schemes, 0);
-	   printExpected("http://www.google.com",true,urlVal.isValid("http://www.google.com"));
-	   System.out.println(urlVal.isValid("http://www.google.com"));
-	   assertTrue(urlVal.isValid("http://www.google.com"));
-	   assertTrue(urlVal.isValid("bad://www.google.com"));
-	   assertTrue(!urlVal.isValid("https://www.google.com"));
+	   int test_fails = 0;
+	   test_fails += printExpected("http://www.google.com",true,urlVal.isValid("http://www.google.com"));
+	   test_fails += printExpected("bad://www.google.com",true,urlVal.isValid("bad://www.google.com"));
+	   test_fails += printExpected("https://www.google.com",false,urlVal.isValid("https://www.google.com"));
+	   assertTrue(test_fails == 0);
+	   
+//	   assertTrue(urlVal.isValid("http://www.google.com"));
+//	   assertTrue(urlVal.isValid("bad://www.google.com"));
+//	   assertTrue(!urlVal.isValid("https://www.google.com"));
 	   
    }
    
   public void testmanualTest_3()
   {   
+	  System.out.println("manual test 3");
 	  UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	  assertTrue(urlVal.isValid("file:///C:/Users/John/Desktop/donoghue_transcript_OSU.html"));
+	  int test_fails = 0;
+	  test_fails += printExpected("file:///C:/Users/John/Desktop/donoghue_transcript_OSU.html",
+			        true,urlVal.isValid("file:///C:/Users/John/Desktop/donoghue_transcript_OSU.html"));
+	  
+	  assertTrue(test_fails == 0);
+	  
+//	  assertTrue(urlVal.isValid("file:///C:/Users/John/Desktop/donoghue_transcript_OSU.html"));
 	    
   }
    
