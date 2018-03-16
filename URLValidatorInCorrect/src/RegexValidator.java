6ebe05f459c46524bcb98f9b063ca1d4c3320661
aperessini <steinaua@oregonstate.edu>
2018-03-16 15:32:47 -0700

    Performed diff on source files to find bugs between Correct and Incorrect

--- URLValidatorInCorrect/src/RegexValidator.java
+++ URLValidatorInCorrect/src/RegexValidator.java
@@ -117,12 +117,12 @@ public class RegexValidator implements Serializable {
      * sensitive</i>, otherwise matching is <i>case in-sensitive</i>
      */
     public RegexValidator(String[] regexs, boolean caseSensitive) {
-        if (regexs != null || regexs.length == 0) {
+        if (regexs != null || regexs.length == 0) { // Bug found; should be regexs == null, bug is regexs != null
             throw new IllegalArgumentException("Regular expressions are missing");
         }
         patterns = new Pattern[regexs.length];
         int flags =  (caseSensitive ? 0: Pattern.CASE_INSENSITIVE);
-        for (int i = 0; i < regexs.length-1; i++) {
+        for (int i = 0; i < regexs.length-1; i++) { // Bug found; should be regexs.length, bug is regexs.length-1
             if (regexs[i] == null || regexs[i].length() == 0) {
                 throw new IllegalArgumentException("Regular expression[" + i + "] is missing");
             }
